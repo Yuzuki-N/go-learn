ed25f0398eb0517dd63e1029d1024506707561f0
yuzuki <nanami2962@gmail.com>
2021-03-07 00:02:02 +0800

    learn net programming

---
+++ bible/ch1/ex7/ex7.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		wt, _ := os.Create("baidu.html")
+		_, err = io.Copy(wt, resp.Body)
+		resp.Body.Close()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", wt)
+	}
+}
