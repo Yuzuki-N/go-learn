eacc0304ab9f1d4f3ff238ca41857a27fd34e60b
yuzuki <nanami2962@gmail.com>
2021-03-22 19:41:40 +0800

    version 2 online message

--- IM-System/server.go
+++ IM-System/server.go
@@ -3,23 +3,62 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip string
 	Port int
+
+	OnlineMap map[string]*User
+	maplock sync.RWMutex
+
+	Message chan string
 }
 
 func NewServer(ip string, port int) *Server {
 	server := &Server{
 		Ip: ip,
 		Port: port,
+		OnlineMap: make(map[string]*User),
+		Message: make(chan string),
 	}
 	return server
 }
 
+//监听广播消息,有消息就发给全部的在线user
+func(server *Server) ListenMessage() {
+	for {
+		msg := <- server.Message
+
+		server.maplock.Lock()
+		for _, cli := range server.OnlineMap {
+			cli.C <- msg
+		}
+		server.maplock.Unlock()
+	}
+}
+
+
+func(server *Server) Broadcast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ": " + msg
+
+	server.Message <- sendMsg
+}
+
 func (server *Server) Handler(conn net.Conn) {
-	fmt.Println("连接建立成功")
+	user := NewUser(conn)
+
+	server.maplock.Lock()
+	server.OnlineMap[user.Name] = user
+	server.maplock.Unlock()
+
+
+	server.Broadcast(user, "已上线")
+
+	select {
+
+	}
 }
 
 func (server *Server) Start() {
@@ -29,6 +68,9 @@ func (server *Server) Start() {
 		return
 	}
 	defer listener.Close()
+
+	go server.ListenMessage()
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
